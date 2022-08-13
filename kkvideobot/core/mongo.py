9a97976b4c7c133672292c55ceb6f3fc0b4f9933
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-08-13 15:11:23 +0530

    Add files via upload

---
+++ kkvideobot/core/mongo.py
@@ -0,0 +1,11 @@
+
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+
+import config
+
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+_mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+
+mongodb = _mongo_async_.Yukki
+pymongodb = _mongo_sync_.Yukki
